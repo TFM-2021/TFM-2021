d65f7f17e8b5e0aa7b16e684a7a00c1327448adf
Ignaciorze <nachorze@gmail.com>
2021-08-11 19:49:14 +0200

    csv comunidades y provincias para incendios. Pruebas paquete Tidygeocoder

--- 02_Data_preparation/02_Construct_data/02_Construct_incendios.R
+++ 02_Data_preparation/02_Construct_data/02_Construct_incendios.R
@@ -1,18 +1,44 @@
+
 library(dplyr)
+library(tidygeocoder)
+library(maps)
+library(ggrepel)
 
 # Datos
 
 datos_incendios <- read.csv(file = './02_Data_preparation/01_Clean_data/incendios_clean.csv')
 
-# datos_comunidades <- read.csv(file = './data/nombre_comunidades.csv')
+datos_comunidades <- read.csv(file = './data/comunidades incendios.csv', 
+                              sep  = ';' , encoding = 'UTF-8')
 
-# datos_comunidades <- read.csv(file = './data/nombre_provincias.csv')
+datos_provincias <- read.csv(file = './data/provincias incendios.csv',  
+                             sep  = ';' , encoding = 'UTF-8')
+
+
+
+datos_incendios <- datos_incendios[,-c(1:2)]
+
+
+
+datos_comunidades <- rename(datos_comunidades, 'idcomunidad' = 'X.U.FEFF.idcomunidad')
+datos_provincias <- rename(datos_provincias, 'idprovincia' = 'X.U.FEFF.idprovincia' )
 
 
 # Merge de dos ficheros para obtener el nombre de la comunidad y de la provincia
 
 
+datos1 <- merge(datos_incendios, datos_comunidades, by = "idcomunidad", all.x = TRUE )
 
+
+datos2 <- merge(datos1, datos_provincias, by = "idprovincia", all.x = TRUE)
+
+datos_finales <- datos2[,-c(1:2)]
+
+
+ggplot(datos_finales, aes(lng, lat), color = "grey99") +
+  borders("state") + geom_point() +
+  geom_label_repel(aes(label = provincia)) +
+  theme_void()
 
 
 # Cambiar las variables de tiempo?
