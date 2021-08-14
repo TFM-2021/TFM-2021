3ffa91798c07212476e2395b1c63460b5cfa5615
sercala97 <sercala97@gmail.com>
2021-08-14 12:48:04 +0200

    añadido el kernel del env y nuevos datos aemet diarios

--- 00_Extraccion_datos/07_aemet_diarios_join_nuevosaños.py
+++ 00_Extraccion_datos/07_aemet_diarios_join_nuevosaños.py
@@ -9,3 +9,32 @@ onlyfiles = [f for f in listdir(r"data/data_raw/AEMET/datos_diario/") if isfile(
 onlyfiles_year = [k for k in onlyfiles if 'year' in k]
 
 print(onlyfiles_year)
+
+
+columnas = ['fecha', 'indicativo', 'nombre', 'provincia', 'altitud', 'tmed', 'prec',
+            'tmin', 'horatmin', 'tmax', 'horatmax', 'dir', 'velmedia', 'racha',     
+            'horaracha', 'sol', 'presMax', 'horaPresMax', 'presMin', 'horaPresMin']
+
+TMP_data_merged = pd.DataFrame(columns=columnas)
+
+for file in onlyfiles_year:
+
+    with open(r'data/data_raw/AEMET/datos_diario/'+ file) as f:
+
+        print(str(file))
+
+        data = json.load(f)
+
+        TMP_data = pd.json_normalize(data)
+
+        TMP_data_merged = TMP_data_merged.append(TMP_data)
+
+TMP_data_merged = TMP_data_merged.reset_index(drop=True)
+
+
+TMP_aemet_diarios_join = pd.read_csv(r"data/data_raw/AEMET/datos_diario/aemet_diarios_join.csv")
+
+TMP_data_merged = TMP_data_merged.append(TMP_aemet_diarios_join)
+
+TMP_data_merged.to_csv(r"data/data_raw/AEMET/datos_diario/aemet_diarios_join_completo.csv", index= False)
+
