5c913dd75dc5c546041c57f412c29f3d91a8697b
sercala97 <sercala97@gmail.com>
2021-08-15 23:17:58 +0200

    inicio preparation ccss

---
+++ 02_Data_preparation/00_Select_data/02_CCSS.R
@@ -0,0 +1,22 @@
+library(readr)
+
+
+expedientes <- read_delim("data/data_raw/CCSS/expedientes.csv", 
+                          ";", escape_double = FALSE, trim_ws = TRUE)
+
+indemnizaciones <- read_delim("data/data_raw/CCSS/indemnizaciones.csv", 
+                              ";", escape_double = FALSE, trim_ws = TRUE)
+
+grandes_eventos <- read_delim("data/data_raw/CCSS/grandes_eventos.csv", 
+                              ";", escape_double = FALSE, trim_ws = TRUE)
+
+grandes_eventos_desglosados <- read_delim("data/data_raw/CCSS/grandes_eventos_desglosados.csv", 
+                                          ";", escape_double = FALSE, trim_ws = TRUE)
+
+View(grandes_eventos_desglosados)
+datos_aemet <- read.csv(file = './data/data_raw/AEMET/datos_diario/aemet_diarios_join_completo.csv')
+
+
+
+grandes_eventos_join <- left_join(grandes_eventos_desglosados, grandes_eventos, "ID")
+View(grandes_eventos_join)
