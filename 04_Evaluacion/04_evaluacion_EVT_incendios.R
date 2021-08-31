cec4f52358c13d846db3e62dcc0ed35914a2c0d2
Ignaciorze <nachorze@gmail.com>
2021-08-31 19:49:28 +0200

    04_Evaluacion_EVT_Incendios

---
+++ 04_Evaluacion/04_evaluacion_EVT_incendios.R
@@ -0,0 +1,118 @@
+
+library(gridExtra)
+
+# CLUSTER 1---------------------------------------------------------------------
+
+
+a <- TFMevt::qq_gev(fit_cluster_1,incendios_cluster_1$mag)
+
+b <- TFMevt::qq_gumbel(fit_cluster_1_Gumbel, incendios_cluster_1$mag)
+
+c <- TFMevt::qq_gpd(fit_cluster_1_GP, incendios_cluster_1$mag,threshold = 3)
+
+
+
+grid.arrange(a, b, c)
+
+# CLUSTER 2-------------------------------------------------------------------
+
+a <- TFMevt::qq_gev(fit_cluster_2,incendios_cluster_2$mag)
+
+b <- TFMevt::qq_gumbel(fit_cluster_2_Gumbel, incendios_cluster_2$mag)
+
+c <- TFMevt::qq_gpd(fit_cluster_2_GP, incendios_cluster_2$mag,threshold = 5)
+c
+
+
+grid.arrange(a, b, c)
+# CLUSTER 3---------------------------------------------------------------------
+
+a <- TFMevt::qq_gev(fit_cluster_3,incendios_cluster_3$mag)
+
+b <- TFMevt::qq_gumbel(fit_cluster_3_Gumbel, incendios_cluster_3$mag)
+
+c <- TFMevt::qq_gpd(fit_cluster_3_GP, incendios_cluster_3$mag,threshold = 3.2)
+a
+b
+c
+
+
+grid.arrange(a, b, c)
+
+# CLUSTER 4---------------------------------------------------------------------
+
+
+
+a <- TFMevt::qq_gev(fit_cluster_4,incendios_cluster_4$mag)
+
+b <- TFMevt::qq_gumbel(fit_cluster_4_Gumbel, incendios_cluster_4$mag)
+
+c <- TFMevt::qq_gpd(fit_cluster_4_GP, incendios_cluster_4$mag,threshold = 3)
+a
+b
+c
+
+
+grid.arrange(a, b, c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
